b67e474787381b61a002bd8f7e040c6b2ba66c13
saurabh-ltim <saurabhkumar.gupta@ltimindtree.com>
2025-04-10 10:41:24 +0530

    Refactored code with CAST recommendations

--- GreetingMessage.java
+++ GreetingMessage.java
@@ -1,26 +1,24 @@
 package com.utc.org1.module1;
 
+import java.util.Arrays;
+
 public class GreetingMessage {
 
     public String buildGreetingMessage(String[] names) {
-
         if (names == null || names.length == 0) {
             return "Hello!";
         }
 
         StringBuilder greeting = new StringBuilder("Hello");
-
-        for (int i = 0; i < names.length; i++) {
-            String name = names[i];
-            if (name != null) {
-                if (i > 0) {
-                    greeting.append(", ");
-                }
-                 greeting.append(name);
-            }
-           
+        for (String name : Arrays.stream(names).map(this::sanitizeInput).toArray(String[]::new)) {
+            greeting.append(", ").append(name);
         }
+        greeting.append("!");
+        return greeting.toString();
+    }
 
-        return greeting.append("!").toString();
+    private String sanitizeInput(String name) {
+        //Implement appropriate sanitization based on OWASP guidelines.  Example below.  Adapt to your needs.
+        return name.replaceAll("[^a-zA-Z0-9\\s]", ""); 
     }
 }
