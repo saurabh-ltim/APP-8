d4f3e37cfe1ad86eb3dedc605a6b199872914d27
saurabh-ltim <saurabhkumar.gupta@ltimindtree.com>
2025-04-13 12:32:48 +0530

    Refactored code with CAST recommendations

--- GreetingMessage.java
+++ GreetingMessage.java
@@ -1,19 +1,18 @@
 package com.utc.org1.module1;
 
+import java.util.Arrays;
+
 public class GreetingMessage {
 
     public String buildGreetingMessage(String[] names) {
-        StringBuilder greeting = new StringBuilder("Hello");
-
-        for (String name : names) {
-            if (name != null) {
-                name = name.trim();
-                if (!name.isEmpty()) {
-                    greeting.append(", ").append(name);
-                }
-            }
+        if (names == null || names.length == 0) {
+            return "Hello!";
         }
 
+        StringBuilder greeting = new StringBuilder("Hello");
+        for (String name : Arrays.stream(names).map(s -> s == null ? "" : s.trim()).toArray(String[]::new)) {
+            greeting.append(", ").append(name);
+        }
         return greeting.append("!").toString();
     }
 }
